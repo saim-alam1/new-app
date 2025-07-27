a6de48539ea0c3903a06d9756c2f444244013441
saim-alam1 <allbehassansaim@gmail.com>
2025-07-27 23:30:25 +0600

    Added Contacts

--- src/pages/ProductPage/ProductPage.jsx
+++ src/pages/ProductPage/ProductPage.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import { MdKeyboardArrowRight } from "react-icons/md";
 import { RiCheckboxCircleLine } from "react-icons/ri";
 import FunctionBtn from "../../shared/FunctionBtn";
+import { FiMapPin } from "react-icons/fi";
+import { FaWhatsapp } from "react-icons/fa";
+import { SiGmail } from "react-icons/si";
 
 const ProductPage = () => {
   return (
@@ -27,7 +30,7 @@ const ProductPage = () => {
           />
         </div>
         {/* Card */}
-        <div className="border border-red-600 rounded-2xl">
+        <div className="border border-red-600 bg-[#FFF6F6] rounded-2xl">
           {/* Card Content */}
           <div className="pt-[18px] px-7">
             <h1 className="font-medium text-4xl">
@@ -46,15 +49,57 @@ const ProductPage = () => {
             <p className="font-bold text-[16px] mb-4">Quantity</p>
             <FunctionBtn />
           </div>
-          {/* Card Bottom */}
+          {/* Card Buttons */}
           <div className="px-7">
-            <input
-              type="text"
-              placeholder="Select your shipping country to see your delivery price
-"
-              className="border border-orange-500 focus:border-orange-500  focus:outline-none py-4 px-3 w-full rounded-[8px] font-medium text-[14px] text-black placeholder:font-medium placeholder:text-black placeholder-text-[14px] my-4"
-            />
+            <div className="relative w-full">
+              <span className="absolute inset-y-0 left-0 bottom-6 flex items-center pl-3">
+                <FiMapPin className="text-black text-lg" />
+              </span>
+              <input
+                type="text"
+                placeholder="Select your shipping country to see your delivery price"
+                className="border border-orange-500 focus:border-orange-500  focus:outline-none py-4 pl-10 pr-3 w-full rounded-[8px] font-medium text-[14px] text-black placeholder:font-medium placeholder:text-black placeholder-text-[14px] mt-4 mb-10"
+              />
+            </div>
+
+            <div className="grid grid-cols-2 gap-4 mb-4">
+              <button className="border border-red-600 rounded-full py-3 font-bold text-[16px] cursor-pointer">
+                Add to Cart
+              </button>
+              <button className="bg-red-600 rounded-full text-white font-bold text-[16px] cursor-pointer">
+                Shop now
+              </button>
+            </div>
           </div>
+        </div>
+      </div>
+
+      {/* Card Info's */}
+      <div className="mt-20 grid grid-cols-1 md:grid-cols-2">
+        <div>
+          <div className="flex items-center">
+            <h5 className="bg-red-600 p-4 rounded-l-full font-bold text-2xl text-white">
+              Product Details
+            </h5>
+            <h5 className="border border-red-600 p-3.5 rounded-r-full text-2xl font-bold">
+              Additional information
+            </h5>
+          </div>
+          <p className="w-[570px] mt-10 text-gray-600">
+            FOR ZANUSSI ELECTROLUX HOOD DYPE DISHWASHER NHT8 , KHT8 , EHT8 ,
+            ZHT8I , AHT8I , EHT8I , ZHT8TI , EHT8TI ZANUSSI TUBO DE SUCÇÃO PARA
+            MÁQUINA DE LAVAR LOUÇA  
+          </p>
+        </div>
+        <div>
+          <button className="flex items-center justify-center gap-2 w-full border border-red-600 py-3 font-bold text-[16px] rounded-full cursor-pointer mb-3.5">
+            <FaWhatsapp className="text-green-500 text-[16px]" /> Send message
+            on What's app
+          </button>
+          <button className="flex items-center justify-center gap-2 w-full border border-red-600 py-3 font-bold text-[16px] rounded-full cursor-pointer">
+            <SiGmail className="text-orange-400 text-[16px]" /> Send message on
+            Email
+          </button>
         </div>
       </div>
     </div>
