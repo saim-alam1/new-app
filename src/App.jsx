ddb9bb0820353d16d4c9d5b22fd07ee7a87d6271
saim-alam1 <allbehassansaim@gmail.com>
2025-07-28 23:52:26 +0600

    Added Cards To Store Page

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import "./App.css";
 import ContactPage from "./pages/ContactPage/ContactPage";
 import ProductPage from "./pages/ProductPage/ProductPage";
 import RelatedProducts from "./pages/ProductPage/RelatedProducts";
+import StorePage from "./pages/StorePage/StorePage";
 
 function App() {
   return (
     <div>
-      {/* <ProductPage />
-      <RelatedProducts /> */}
-      <ContactPage />
+      {/* <ProductPage /> */}
+      {/* <RelatedProducts /> */}
+      {/* <ContactPage /> */}
+      <StorePage />
     </div>
   );
 }
---
+++ src/pages/StorePage/StorePage.jsx
@@ -0,0 +1,276 @@
+import { FaRegCheckCircle } from "react-icons/fa";
+
+const StorePage = () => {
+  return (
+    <div className="max-w-[1280px] mx-auto px-3">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-16 mb-20">
+        {/* Card 1 */}
+        <div className="bg-white border border-red-600 p-3 rounded-2xl">
+          <div className="relative">
+            <img
+              src="https://i.ibb.co/hRzFKvnD/60e0638a76288d19f154fb8f9b723f09e1918dad.png"
+              alt="Item Image"
+            />
+            <p className="text-green-600 text-2xl font-bold flex items-center gap-2 absolute z-10 top-0">
+              <FaRegCheckCircle className="" />
+              In Stock
+            </p>
+          </div>
+          <p className="font-medium text-2xl mb-2">
+            ZANUSSI HOOD DISHWASHER OWERFLOW PIPE WITH FILTER
+          </p>
+          {/* Pricing */}
+          <div className="flex items-center gap-2">
+            <p className="font-bold text-xl">$48.33</p>
+            <p className="font-medium text-[14px] text-gray-500 line-through">
+              $295.05
+            </p>
+          </div>
+          {/* Buttons */}
+          <div className="grid grid-cols-2 gap-4 mt-7">
+            <button className="font-bold text-[16px] text-red-600 border border-red-600 rounded-full py-3 cursor-pointer">
+              View Details
+            </button>
+            <button className="font-bold text-[16px] text-white bg-red-600 rounded-full py-3 cursor-pointer">
+              Add to Cart
+            </button>
+          </div>
+        </div>
+        {/* Card 2 */}
+        <div className="bg-white border border-red-600 p-3 rounded-2xl">
+          <img
+            src="https://i.ibb.co/hRzFKvnD/60e0638a76288d19f154fb8f9b723f09e1918dad.png"
+            alt="Item Image"
+          />
+          <p className="font-medium text-2xl mb-2">
+            ZANUSSI HOOD DISHWASHER OWERFLOW PIPE WITH FILTER
+          </p>
+          {/* Pricing */}
+          <div className="flex items-center gap-2">
+            <p className="font-bold text-xl">$48.33</p>
+            <p className="font-medium text-[14px] text-gray-500 line-through">
+              $295.05
+            </p>
+          </div>
+          {/* Buttons */}
+          <div className="grid grid-cols-2 gap-4 mt-7">
+            <button className="font-bold text-[16px] text-red-600 border border-red-600 rounded-full py-3 cursor-pointer">
+              View Details
+            </button>
+            <button className="font-bold text-[16px] text-white bg-red-600 rounded-full py-3 cursor-pointer">
+              Add to Cart
+            </button>
+          </div>
+        </div>
+        {/* Card 3 */}
+        <div className="bg-white border border-red-600 p-3 rounded-2xl">
+          <div className="relative">
+            <img
+              src="https://i.ibb.co/hRzFKvnD/60e0638a76288d19f154fb8f9b723f09e1918dad.png"
+              alt="Item Image"
+            />
+            <p className="text-green-600 text-2xl font-bold flex items-center gap-2 absolute z-10 top-0">
+              <FaRegCheckCircle className="" />
+              In Stock
+            </p>
+          </div>
+          <p className="font-medium text-2xl mb-2">
+            ZANUSSI HOOD DISHWASHER OWERFLOW PIPE WITH FILTER
+          </p>
+          {/* Pricing */}
+          <div className="flex items-center gap-2">
+            <p className="font-bold text-xl">$48.33</p>
+            <p className="font-medium text-[14px] text-gray-500 line-through">
+              $295.05
+            </p>
+          </div>
+          {/* Buttons */}
+          <div className="grid grid-cols-2 gap-4 mt-7">
+            <button className="font-bold text-[16px] text-red-600 border border-red-600 rounded-full py-3 cursor-pointer">
+              View Details
+            </button>
+            <button className="font-bold text-[16px] text-white bg-red-600 rounded-full py-3 cursor-pointer">
+              Add to Cart
+            </button>
+          </div>
+        </div>
+        {/* Card 4 */}
+        <div className="bg-white border border-red-600 p-3 rounded-2xl">
+          <div className="relative">
+            <img
+              src="https://i.ibb.co/hRzFKvnD/60e0638a76288d19f154fb8f9b723f09e1918dad.png"
+              alt="Item Image"
+            />
+            <p className="text-green-600 text-2xl font-bold flex items-center gap-2 absolute z-10 top-0">
+              <FaRegCheckCircle className="" />
+              In Stock
+            </p>
+          </div>
+          <p className="font-medium text-2xl mb-2">
+            ZANUSSI HOOD DISHWASHER OWERFLOW PIPE WITH FILTER
+          </p>
+          {/* Pricing */}
+          <div className="flex items-center gap-2">
+            <p className="font-bold text-xl">$48.33</p>
+            <p className="font-medium text-[14px] text-gray-500 line-through">
+              $295.05
+            </p>
+          </div>
+          {/* Buttons */}
+          <div className="grid grid-cols-2 gap-4 mt-7">
+            <button className="font-bold text-[16px] text-red-600 border border-red-600 rounded-full py-3 cursor-pointer">
+              View Details
+            </button>
+            <button className="font-bold text-[16px] text-white bg-red-600 rounded-full py-3 cursor-pointer">
+              Add to Cart
+            </button>
+          </div>
+        </div>
+        {/* Card 5 */}
+        <div className="bg-white border border-red-600 p-3 rounded-2xl">
+          <div className="relative">
+            <img
+              src="https://i.ibb.co/hRzFKvnD/60e0638a76288d19f154fb8f9b723f09e1918dad.png"
+              alt="Item Image"
+            />
+            <p className="text-green-600 text-2xl font-bold flex items-center gap-2 absolute z-10 top-0">
+              <FaRegCheckCircle className="" />
+              In Stock
+            </p>
+          </div>
+          <p className="font-medium text-2xl mb-2">
+            ZANUSSI HOOD DISHWASHER OWERFLOW PIPE WITH FILTER
+          </p>
+          {/* Pricing */}
+          <div className="flex items-center gap-2">
+            <p className="font-bold text-xl">$48.33</p>
+            <p className="font-medium text-[14px] text-gray-500 line-through">
+              $295.05
+            </p>
+          </div>
+          {/* Buttons */}
+          <div className="grid grid-cols-2 gap-4 mt-7">
+            <button className="font-bold text-[16px] text-red-600 border border-red-600 rounded-full py-3 cursor-pointer">
+              View Details
+            </button>
+            <button className="font-bold text-[16px] text-white bg-red-600 rounded-full py-3 cursor-pointer">
+              Add to Cart
+            </button>
+          </div>
+        </div>
+        {/* Card 6 */}
+        <div className="bg-white border border-red-600 p-3 rounded-2xl">
+          <img
+            src="https://i.ibb.co/hRzFKvnD/60e0638a76288d19f154fb8f9b723f09e1918dad.png"
+            alt="Item Image"
+          />
+          <p className="font-medium text-2xl mb-2">
+            ZANUSSI HOOD DISHWASHER OWERFLOW PIPE WITH FILTER
+          </p>
+          {/* Pricing */}
+          <div className="flex items-center gap-2">
+            <p className="font-bold text-xl">$48.33</p>
+            <p className="font-medium text-[14px] text-gray-500 line-through">
+              $295.05
+            </p>
+          </div>
+          {/* Buttons */}
+          <div className="grid grid-cols-2 gap-4 mt-7">
+            <button className="font-bold text-[16px] text-red-600 border border-red-600 rounded-full py-3 cursor-pointer">
+              View Details
+            </button>
+            <button className="font-bold text-[16px] text-white bg-red-600 rounded-full py-3 cursor-pointer">
+              Add to Cart
+            </button>
+          </div>
+        </div>
+        {/* Card 7 */}
+        <div className="bg-white border border-red-600 p-3 rounded-2xl">
+          <div className="relative">
+            <img
+              src="https://i.ibb.co/hRzFKvnD/60e0638a76288d19f154fb8f9b723f09e1918dad.png"
+              alt="Item Image"
+            />
+            <p className="text-green-600 text-2xl font-bold flex items-center gap-2 absolute z-10 top-0">
+              <FaRegCheckCircle className="" />
+              In Stock
+            </p>
+          </div>
+          <p className="font-medium text-2xl mb-2">
+            ZANUSSI HOOD DISHWASHER OWERFLOW PIPE WITH FILTER
+          </p>
+          {/* Pricing */}
+          <div className="flex items-center gap-2">
+            <p className="font-bold text-xl">$48.33</p>
+            <p className="font-medium text-[14px] text-gray-500 line-through">
+              $295.05
+            </p>
+          </div>
+          {/* Buttons */}
+          <div className="grid grid-cols-2 gap-4 mt-7">
+            <button className="font-bold text-[16px] text-red-600 border border-red-600 rounded-full py-3 cursor-pointer">
+              View Details
+            </button>
+            <button className="font-bold text-[16px] text-white bg-red-600 rounded-full py-3 cursor-pointer">
+              Add to Cart
+            </button>
+          </div>
+        </div>
+        {/* Card 8 */}
+        <div className="bg-white border border-red-600 p-3 rounded-2xl">
+          <img
+            src="https://i.ibb.co/hRzFKvnD/60e0638a76288d19f154fb8f9b723f09e1918dad.png"
+            alt="Item Image"
+          />
+          <p className="font-medium text-2xl mb-2">
+            ZANUSSI HOOD DISHWASHER OWERFLOW PIPE WITH FILTER
+          </p>
+          {/* Pricing */}
+          <div className="flex items-center gap-2">
+            <p className="font-bold text-xl">$48.33</p>
+            <p className="font-medium text-[14px] text-gray-500 line-through">
+              $295.05
+            </p>
+          </div>
+          {/* Buttons */}
+          <div className="grid grid-cols-2 gap-4 mt-7">
+            <button className="font-bold text-[16px] text-red-600 border border-red-600 rounded-full py-3 cursor-pointer">
+              View Details
+            </button>
+            <button className="font-bold text-[16px] text-white bg-red-600 rounded-full py-3 cursor-pointer">
+              Add to Cart
+            </button>
+          </div>
+        </div>
+        {/* Card 9 */}
+        <div className="bg-white border border-red-600 p-3 rounded-2xl">
+          <img
+            src="https://i.ibb.co/hRzFKvnD/60e0638a76288d19f154fb8f9b723f09e1918dad.png"
+            alt="Item Image"
+          />
+          <p className="font-medium text-2xl mb-2">
+            ZANUSSI HOOD DISHWASHER OWERFLOW PIPE WITH FILTER
+          </p>
+          {/* Pricing */}
+          <div className="flex items-center gap-2">
+            <p className="font-bold text-xl">$48.33</p>
+            <p className="font-medium text-[14px] text-gray-500 line-through">
+              $295.05
+            </p>
+          </div>
+          {/* Buttons */}
+          <div className="grid grid-cols-2 gap-4 mt-7">
+            <button className="font-bold text-[16px] text-red-600 border border-red-600 rounded-full py-3 cursor-pointer">
+              View Details
+            </button>
+            <button className="font-bold text-[16px] text-white bg-red-600 rounded-full py-3 cursor-pointer">
+              Add to Cart
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default StorePage;
