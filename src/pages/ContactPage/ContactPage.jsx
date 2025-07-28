13a2dbd171d9a896fc17f5dd47686fff3fc6cc2a
saim-alam1 <allbehassansaim@gmail.com>
2025-07-28 16:50:30 +0600

    Contact Page Second Part Cards

--- src/pages/ContactPage/ContactPage.jsx
+++ src/pages/ContactPage/ContactPage.jsx
@@ -1,3 +1,7 @@
+import { FiPhone } from "react-icons/fi";
+import { HiOutlineMail } from "react-icons/hi";
+import { IoLocationOutline } from "react-icons/io5";
+
 const ContactPage = () => {
   return (
     <div className="max-w-[1280px] mx-auto px-3">
@@ -22,6 +26,118 @@ const ContactPage = () => {
           >
             If you need help, don't hesitate to contact our support team!
           </h1>
+        </div>
+
+        {/* Card Content */}
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-20">
+          {/* Card 1 */}
+          <div className="bg-[#f4f3ef] rounded-xl">
+            <div className="flex items-center justify-center gap-2.5 py-4 border-b border-gray-300">
+              <span className="bg-red-600 p-1.5 rounded-[8px]">
+                <FiPhone className="text-xl text-white" />
+              </span>
+              <p className="text-xl font-bold">Number</p>
+            </div>
+
+            <div className="flex flex-col items-center justify-center py-28 space-y-4 text-left">
+              <p className=" text-[16px] font-bold text-red-600">
+                <span className="text-[#767778]">WhatsApp</span> +351 927408959
+              </p>
+              <p className=" text-[16px] font-bold text-red-600">
+                <span className="text-[#767778]">Office number</span> +351
+                275335326
+              </p>
+              <p className=" text-[16px] font-bold text-red-600">
+                <span className="text-[#767778]">Mob office</span> +351
+                927408959
+              </p>
+            </div>
+          </div>
+
+          {/* Card 2 */}
+          <div className="bg-[#f4f3ef] rounded-xl">
+            <div className="flex items-center justify-center gap-2.5 py-4 border-b border-gray-300">
+              <span className="bg-red-600 p-1.5 rounded-[8px]">
+                <HiOutlineMail className="text-xl text-white" />
+              </span>
+              <p className="text-xl font-bold">Email Us</p>
+            </div>
+
+            <div className="grid grid-cols-2 gap-3 pt-[26px] px-[70px]">
+              {/* Side 1 */}
+              <div className="space-y-4">
+                <p className=" text-[16px] font-bold text-red-600">
+                  <span className="text-[#767778]">Import Department -</span>{" "}
+                  import@hdotrade.com
+                </p>
+                <p className=" text-[16px] font-bold text-red-600">
+                  <span className="text-[#767778]">
+                    Sales Department Global -
+                  </span>{" "}
+                  sales@hdotrade.com
+                </p>
+                <p className=" text-[16px] font-bold text-red-600">
+                  <span className="text-[#767778]">
+                    Sales Department Europ -
+                  </span>{" "}
+                  sales@Hdotrade.pt
+                </p>
+              </div>
+              {/* Side 2 */}
+              <div className="space-y-4">
+                <p className=" text-[16px] font-bold text-red-600">
+                  <span className="text-[#767778]">
+                    Sccountancy Department Global -
+                  </span>{" "}
+                  accountancy@hdotrade.com
+                </p>
+                <p className=" text-[16px] font-bold text-red-600">
+                  <span className="text-[#767778]">
+                    Accountancy Department Europ -
+                  </span>{" "}
+                  accountancy@hdotrade.pt
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-20">
+          {/* Side 1 */}
+          <div className="bg-[#f4f3ef] rounded-xl">
+            <div className="flex items-center justify-center gap-2.5 py-4 border-b border-gray-300">
+              <span className="bg-red-600 p-1.5 rounded-[8px]">
+                <IoLocationOutline className="text-xl text-white" />
+              </span>
+              <p className="text-xl font-bold">
+                Offices, warehouse, physical store
+              </p>
+            </div>
+
+            <div className="flex flex-col items-center justify-center pt-4 pb-7 space-y-4 text-center">
+              <p className="text-[16px] font-bold text-[#767779]">
+                Quinta das rosas lote 3 RC Esq 6200-551 Covilha Portugal
+              </p>
+              <p className="text-[16px] font-bold text-[#767779]">
+                warehouse - rua de espinho santo peraboa covilha <br />
+                portugalimport@hdotrade.com
+              </p>
+            </div>
+          </div>
+          {/* Side 2 */}
+          <div className="bg-[#f4f3ef] rounded-xl">
+            <div className="flex items-center justify-center gap-2.5 py-4 border-b border-gray-300">
+              <span className="bg-red-600 p-1.5 rounded-[8px]">
+                <FiPhone className="text-xl text-white" />
+              </span>
+              <p className="text-xl font-bold">Office Hours</p>
+            </div>
+
+            <div className="flex flex-col items-center justify-center text-center">
+              <p className="text-[16px] font-bold text-red-600 py-11">
+                Monday - Friday 9 AM - 6 PM
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
