ca4661506f82fed29443db6318183ae9d4e7e43c
saim-alam1 <allbehassansaim@gmail.com>
2025-07-28 15:09:46 +0600

    Added Header In Contact Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
+import ContactPage from "./pages/ContactPage/ContactPage";
 import ProductPage from "./pages/ProductPage/ProductPage";
 import RelatedProducts from "./pages/ProductPage/RelatedProducts";
 
 function App() {
   return (
     <div>
-      <ProductPage />
-      <RelatedProducts />
+      {/* <ProductPage />
+      <RelatedProducts /> */}
+      <ContactPage />
     </div>
   );
 }
